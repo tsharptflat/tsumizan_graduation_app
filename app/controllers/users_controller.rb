f1d286025be106866e315d9dc7ba21966dd67a3a
tsharptflat <tsharptflat@gmail.com>
2026-04-27 23:51:09 +0900

    fix:users_fat_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,8 @@
 class UsersController < ApplicationController
-  def loading
-  end
-
   def show
     @page = 'users_show'
     @total_price = current_user.user_game_libraries.unplayed.joins(:game).sum('games.price')
-    @character_text = get_character_text(@page, @total_price)
+    @character_text = CharacterTextService.new.get_character_text(current_user, @page, @total_price)
     @character_expression = @character_text.character_expression
-  end
-
-  private
-
-  def get_character_text(page, total_price)
-    user = current_user
-    user_character = user.user_characters.first
-    character_type = user_character.character_type
-    condition = CharacterTextCondition.where(character_type_id: character_type.id).where(page: page).where('min_price <= ? AND (max_price IS NULL OR max_price >= ? )', total_price, total_price).first
-    text = condition.character_texts.first
   end
 end
