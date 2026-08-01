279fe23aec468c7ef185419e3005488d69d33eff
tsharptflat <tsharptflat@gmail.com>
2026-08-01 18:13:41 +0900

    add:計算中の間はキャラクターのセリフ及び表情を変更するよう調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
     @user = current_user
     @page = 'users_show'
     @total_price = UserGameLibrary.total_price(current_user)
-    @character_text = CharacterTextService.new.get_character_text(current_user.user_characters.first, @page, @total_price)
+    text_page = UserGameLibrary.any_library_game_prices_nil?(current_user) ? 'users_show_loading' : @page
+    @character_text = CharacterTextService.new.get_character_text(current_user.user_characters.first, text_page, @total_price)
     @character_expression = @character_text.character_expression
   end
 
