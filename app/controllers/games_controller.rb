6c07c4dd4e2922e668e3819d1121d0522feeeccd
tsharptflat <tsharptflat@gmail.com>
2026-08-02 15:54:03 +0900

    add:ゲームごとの詳細ページ

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,16 +5,13 @@ class GamesController < ApplicationController
 
     def show
         @user = current_user
-        所持者数
-        積み率
-        クリア率
-        自分の積み/クリア状況
-        平均プレイ時間
-        自分のプレイ時間
-        平均積み期間
-        自分の積み時間
-        ジャンル
-        価格
-        ストアリンク
+        @game = Game.find(params[:id])
+        @possessed_user_count = UserGameLibrary.game_statistics_count(@game)
+        @unplayed_percentage = UserGameLibrary.game_statistics_unplayed_percentage(@game)
+        @cleared_percentage = UserGameLibrary.game_statistics_cleared_rate(@game)
+        @average_playtime = UserGameLibrary.game_statistics_average_playtime(@game)
+        @average_unplayed_time = UserGameLibrary.game_statistics_average_unplayed_time(@game)
+
+        @current_user_game = @user.user_game_libraries.find_by(game_id: @game.id)
     end
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,9 @@
 module GamesHelper
+  def steam_store_link(game)
+    "https://store.steampowered.com/app/#{game.steam_app_id}"
+  end
+
+  def steam_banner_image(game)
+    "https://cdn.akamai.steamstatic.com/steam/apps/#{game.steam_app_id}/header.jpg"
+  end
 end
