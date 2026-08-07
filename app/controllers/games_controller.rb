903ba73205fa5895ef7ab0e0debe65f39ebf3022
tsharptflat <tsharptflat@gmail.com>
2026-08-07 23:54:41 +0900

    add:ゲーム検索機能(ransack導入)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,22 @@
 class GamesController < ApplicationController
     def  index
+        @q = Game.ransack(params[:q])
         @all_games_count = UserGameLibrary.all_games_count
         @all_unplayed_games_count = UserGameLibrary.all_unplayed_games_count
 
         @games = case params[:sort]
         when "name"
-            Game.order(:game_title).page(params[:page])
+            @q.result(distinct: true).order(:game_title).page(params[:page])
         when "price_asc"
-            Game.order(price: :asc).page(params[:page])
+            @q.result(distinct: true).order(price: :asc).page(params[:page])
         when "price_desc"
-            Game.order(price: :desc).page(params[:page])
+            @q.result(distinct: true).order(price: :desc).page(params[:page])
         when "possessed_count"
-            Kaminari.paginate_array(Game.all.sort_by{ |game| -(@all_games_count.fetch(game.id, 0)) }).page(params[:page])
+            Kaminari.paginate_array(@q.result(distinct: true).all.sort_by{ |game| -(@all_games_count.fetch(game.id, 0)) }).page(params[:page])
         when "unplayed_rate"
-            Kaminari.paginate_array(Game.all.sort_by{ |game| -(@all_unplayed_games_count.fetch(game.id, 0).to_f / @all_games_count.fetch(game.id, 0).to_f) }).page(params[:page])
+            Kaminari.paginate_array(@q.result(distinct: true).all.sort_by{ |game| -(@all_unplayed_games_count.fetch(game.id, 0).to_f / @all_games_count.fetch(game.id, 0).to_f) }).page(params[:page])
         else
-            Kaminari.paginate_array(Game.all.sort_by{ |game| -(@all_games_count.fetch(game.id, 0)) }).page(params[:page])
+            Kaminari.paginate_array(@q.result(distinct: true).all.sort_by{ |game| -(@all_games_count.fetch(game.id, 0)) }).page(params[:page])
         end
     end
 
